edcaca125214f02a13c4f319b5198f6a448e88b0
krthr <twilson@uninorte.edu.co>
2021-04-01 15:47:10 -0500

    feat(): Navbar & pages (draft)

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,17 @@
 <template>
-  <nav class="navbar justify-center flex-wrap py-6">
+  <nav class="navbar justify-center flex-wrap py-6 space-x-3">
     <nuxt-link class="btn" to="/"> Home </nuxt-link>
-    <nuxt-link class="btn" to="/"> Blog </nuxt-link>
-    <nuxt-link class="btn" to="/"> Contact </nuxt-link>
+    <nuxt-link class="btn" to="/blog"> Blog </nuxt-link>
+    <nuxt-link class="btn" to="/contact"> Contact </nuxt-link>
   </nav>
 </template>
+
+<style scoped>
+.btn {
+  @apply btn-sm btn-ghost;
+}
+
+.btn.nuxt-link-exact-active {
+  @apply btn-primary;
+}
+</style>
