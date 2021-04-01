433c9f482af80ed9fb0829816718fcfaa6e396ed
krthr <twilson@uninorte.edu.co>
2021-04-01 16:23:53 -0500

    feat(): contact page

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <Navbar></Navbar>
 
-    <main class="container m-auto">
+    <main class="container m-auto w-5/6 md:w-4/5 lg:w-2/5">
       <Nuxt />
     </main>
   </div>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,69 @@
 <template>
-  <div>contact</div>
+  <div>
+    <form @submit.prevent="submit">
+      <div class="form-control">
+        <input
+          v-model="form.name"
+          type="text"
+          placeholder="Name"
+          class="input input-bordered"
+        />
+      </div>
+
+      <div class="form-control">
+        <input
+          v-model="form.subject"
+          type="text"
+          placeholder="Subject"
+          class="input input-bordered"
+        />
+      </div>
+
+      <div class="form-control">
+        <input
+          v-model="form.email"
+          type="email"
+          placeholder="Email"
+          class="input input-bordered"
+        />
+      </div>
+
+      <div class="form-control">
+        <textarea
+          v-model="form.message"
+          class="textarea h-24 textarea textarea-bordered"
+          placeholder="Message"
+        ></textarea>
+      </div>
+
+      <div class="form-control">
+        <button class="btn btn-primary" type="submit">Enviar</button>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    form: {
+      name: '',
+      subject: '',
+      email: '',
+      message: '',
+    },
+  }),
+
+  methods: {
+    submit() {
+      // TODO
+    },
+  },
+}
+</script>
+
+<style scoped>
+.form-control {
+  @apply mb-3;
+}
+</style>
