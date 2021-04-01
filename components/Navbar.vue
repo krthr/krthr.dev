eb4be9ea84cf07782a8bbc2c99237f0058f5c132
krthr <twilson@uninorte.edu.co>
2021-04-01 15:42:38 -0500

    feat(): Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar justify-center flex-wrap py-6">
+    <nuxt-link class="btn" to="/"> Home </nuxt-link>
+    <nuxt-link class="btn" to="/"> Blog </nuxt-link>
+    <nuxt-link class="btn" to="/"> Contact </nuxt-link>
+  </nav>
+</template>
