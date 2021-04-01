80887dfd012d896997b2ba0a6cb9fbe22987cd5a
krthr <twilson@uninorte.edu.co>
2021-04-01 15:27:45 -0500

    feat(): @nuxtjs/web-vitals

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ['@nuxtjs/tailwindcss'],
+  buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/web-vitals'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
